e016f8f5cc04b6288703d56a44ac6ba264e6a0b8
justfishin1999 <107942195+justfishin1999@users.noreply.github.com>
2025-05-22 18:10:35 -0500

    fix issue with arena notifiers in relation to a/e stop trip

--- field/arena_notifiers.go
+++ field/arena_notifiers.go
@@ -368,10 +368,14 @@ func getRulesViolated(redFouls, blueFouls []game.Foul) map[int]*game.Rule {
 
 // notifies websocket of changes in a/e stop outside of match
 func (arena *Arena) NotifyStationTripStatus() {
-	matchInProgress := arena.MatchState == MatchInProgress
+	matchInProgress := arena.MatchState != PreMatch &&
+		arena.MatchState != StartMatch &&
+		arena.MatchState != PostMatch &&
+		arena.MatchState != TimeoutActive &&
+		arena.MatchState != PostTimeout
 
 	for id, station := range arena.AllianceStations {
-		arena.StationTripNotifier.Notify(map[string]any{
+		arena.StationTripNotifier.NotifyWithMessage(map[string]any{
 			"StationId":        id,
 			"AStopTripped":     station.AStop,
 			"EStopTripped":     station.EStop,
